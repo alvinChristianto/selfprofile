8f6a0ecbf3a223900d264424c3aa9cf9db555528
alvinChristianto <alvin@thortech.asia>
2023-06-28 10:55:10 +0700

    move head info to page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,38 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>AlvinC | IT enthusiast</title>
-        <meta name="title" content="AlvinC | IT enthusiast" />
-        <meta
-          name="description"
-          content="Alvin Christianto, Business analyst, Frontend and Blockchain developer "
-        />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://alvinch.vercel.app/" />
-        <meta property="og:title" content="AlvinC | IT enthusiast" />
-        <meta
-          property="og:description"
-          content="Alvin Christianto, Business analyst, Frontend and Blockchain developer "
-        />
-        <meta
-          property="og:image"
-          content="https://alvinch.vercel.app/_next/image?url=%2Fimg%2Fimage_alvin.png&w=256&q=75&dpl=dpl_FgdxTokHeyNVbrA36DYNqhnHw3DB"
-        />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://alvinch.vercel.app/" />
-        <meta property="twitter:title" content="AlvinC | IT enthusiast" />
-        <meta
-          property="twitter:description"
-          content="Alvin Christianto, Business analyst, Frontend and Blockchain developer "
-        />
-        <meta
-          property="twitter:image"
-          content="https://alvinch.vercel.app/_next/image?url=%2Fimg%2Fimage_alvin.png&w=256&q=75&dpl=dpl_FgdxTokHeyNVbrA36DYNqhnHw3DB"
-        />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
